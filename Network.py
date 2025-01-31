0e42b12f065abcd1ecb306104bf46b0ad83fe488
DHC <dariohcvg@gmail.com>
2025-01-31 14:47:44 -0500

    Several Changes
    * Fix several things in Network.py
    * Create Testing Script
    * Add README

--- Network.py
+++ Network.py
@@ -4,7 +4,6 @@ from enum import Enum
 from non_linearity_picker import *
 import random
 
-MOMENTUM_ALPHA = .9
 
 class Optimizers(Enum):
     nag = 'nag'
@@ -29,10 +28,26 @@ class Network:
                 normalvariate(0, variance) for i in range(nodes)
             ]) for nodes in layers[1:]
         ]
+
+    def serialize(self):    
+        return {
+            'layers': self.layers,
+            'weights': [w.tolist() for w in self.weights],
+            'biases': [b.tolist() for b in self.biases],
+            'non_linearity_function': self.non_linearity_function.value
+        }
+    
+    def deserialize(data: dict):
+        return Network(
+            data['layers'],
+            weights=[np.array(w) for w in data['weights']],
+            biases=[np.array(b) for b in data['biases']],
+            non_linearity_function=Non_Linearity_Functions(data['non_linearity_function']) 
+        )
     
     def backprop(self, inputs: np.ndarray, outputs: np.ndarray):
-        nabla_b = [np.zeros(nodes) for nodes in self.layers[1:]]
-        nabla_w = [np.zeros((nodes, nodes_left)) for nodes_left, nodes in zip(self.layers[1:], self.layers[:-1])]
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
         
         ## forward pass
         activations = [inputs]
@@ -63,34 +78,19 @@ class Network:
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
         
-        # print([nb.shape for nb in nabla_b])
-        # print([nw.shape for nw in nabla_w])
-        # print('-----------------')
         for input, output in batch:
             nabla_b_i, nabla_w_i = self.backprop(input, output)
 
             for i, pair in enumerate(zip(nabla_b_i, nabla_w_i)):
                 bi, wi = pair
-
-                # print(f"{nabla_b[i].shape} + {bi.shape}")
-                # print(f"{nabla_w[i].shape} + {wi.shape}")
-
         
                 nabla_b[i] = nabla_b[i] + bi.reshape(len(bi))
                 nabla_w[i] = nabla_w[i] + wi
 
-                # print()
-
-        # print('-----------------')
-        
-        # print([nb.shape for nb in nabla_b])
-        # print([nw.shape for nw in nabla_w])
-
-
         n = len(batch)
-        for i in range(len(self.layers) - 1):
-            nabla_b[i]/=n
-            nabla_w[i]/=n
+        for i in range(len(nabla_b)):
+            nabla_b[i] = nabla_b[i] / n
+            nabla_w[i] = nabla_w[i] / n
 
         return (nabla_b, nabla_w)
 
@@ -100,21 +100,21 @@ class Network:
             self.weights[i] = self.weights[i] - nabla_w[i]*eta
             self.biases[i] = self.biases[i] - nabla_b[i]*eta
 
-    def momentum_epoch_apply(self, mini_batch: list[(np.ndarray, np.ndarray)], momentum_b: list[np.ndarray], momentum_w: list[np.ndarray], eta: float):
+    def momentum_epoch_apply(self, mini_batch: list[(np.ndarray, np.ndarray)], momentum_b: list[np.ndarray], momentum_w: list[np.ndarray], eta: float, momentum_alpha: float = .9):
         nabla_b, nabla_w = self.gradient_mini_batch(mini_batch)
         for i in range(len(self.layers) - 1):
-            nabla_w[i] = -nabla_w[i]*eta + momentum_w[i]*MOMENTUM_ALPHA
-            nabla_b[i] = -nabla_b[i]*eta + momentum_b[i]*MOMENTUM_ALPHA
+            nabla_w[i] = -nabla_w[i]*eta + momentum_w[i]*momentum_alpha
+            nabla_b[i] = -nabla_b[i]*eta + momentum_b[i]*momentum_alpha
 
             self.weights[i] = self.weights[i] + nabla_w[i]
             self.biases[i] = self.biases[i] + nabla_b[i]
 
         return (nabla_b, nabla_w)
 
-    def nag_epoch_apply(self, mini_batch: list[(np.ndarray, np.ndarray)], momentum_b: list[np.ndarray], momentum_w: list[np.ndarray], eta: float):
+    def nag_epoch_apply(self, mini_batch: list[(np.ndarray, np.ndarray)], momentum_b: list[np.ndarray], momentum_w: list[np.ndarray], eta: float, momentum_alpha: float = .9):
         for i in range(len(self.layers) - 1):
-            self.weights[i] = self.weights[i] + momentum_w[i]*MOMENTUM_ALPHA
-            self.biases[i] = self.biases[i] + momentum_b[i]*MOMENTUM_ALPHA
+            self.weights[i] = self.weights[i] + momentum_w[i]*momentum_alpha
+            self.biases[i] = self.biases[i] + momentum_b[i]*momentum_alpha
 
         nabla_b, nabla_w = self.gradient_mini_batch(mini_batch)
 
@@ -122,24 +122,30 @@ class Network:
             self.weights[i] = self.weights[i] - nabla_w[i]*eta
             self.biases[i] = self.biases[i] - nabla_b[i]*eta
 
-            nabla_w[i] = momentum_w[i]*MOMENTUM_ALPHA - nabla_w[i]*eta
-            nabla_b[i] = momentum_b[i]*MOMENTUM_ALPHA - nabla_b[i]*eta
+            nabla_w[i] = momentum_w[i]*momentum_alpha - nabla_w[i]*eta
+            nabla_b[i] = momentum_b[i]*momentum_alpha - nabla_b[i]*eta
 
         return (nabla_b, nabla_w)
 
-    def SGD_train(self, data: list[(np.ndarray, np.ndarray)], epochs: int, eta: float = 0.01, mini_batch_size: int = 32, sgd_sample_replacement: bool = False, optimizer: Optimizers = Optimizers.nag):
+    def SGD_train(self, data: list[(np.ndarray, np.ndarray)], epochs: int, eta: float = 0.01, mini_batch_size: int = 32, sgd_sample_replacement: bool = False, optimizer: Optimizers = Optimizers.constant_eta, test: list[(np.ndarray, np.ndarray)] = None, name: str = None):
         def get_mini_batches():
             if sgd_sample_replacement:
                 mini_batches = [
                     random.choices(data, k=mini_batch_size) for i in range(int(np.ceil(len(data)/mini_batch_size)))
                 ]
             else:
-                random.shuffle(data)
+                data_copy = data.copy()
+                random.shuffle(data_copy)
                 mini_batches = [
                     data[k: k + mini_batch_size] for k in range(0, len(data), mini_batch_size)
                 ]
 
             return mini_batches
+        
+        def complete_epoch(i):
+            print(f"Epoch {i + 1} Completed" + (f" for {name}" if name else ''))
+            if test:
+                print(f"Accuracy: {self.evaluate(test)}" + (f" for {name}" if name else ''))
 
         if optimizer == Optimizers.constant_eta:
             for i in range(epochs):
@@ -147,18 +153,23 @@ class Network:
                 for batch in mini_batches:
                     self.constant_eta_epoch_apply(batch, eta)
 
-                print(f"Epoch {i + 1} Completed")
+                complete_epoch(i)
 
         elif optimizer == Optimizers.momentum:
             momentum_b = [np.zeros(b.shape) for b in self.biases]
             momentum_w = [np.zeros(w.shape) for w in self.weights]
 
+            momentum_alpha = .9
+            m_dec = (.9 - .1)/epochs
+            
             for i in range(epochs):
                 mini_batches = get_mini_batches()
+                momentum_alpha -= m_dec
+                
                 for batch in mini_batches:
-                    momentum_b, momentum_w = self.momentum_epoch_apply(batch, momentum_b, momentum_w, eta)
+                    momentum_b, momentum_w = self.momentum_epoch_apply(batch, momentum_b, momentum_w, eta, momentum_alpha=momentum_alpha)
 
-                print(f"Epoch {i + 1} Completed")
+                complete_epoch(i)
 
         elif optimizer == Optimizers.nag:
             momentum_b = [np.zeros(b.shape) for b in self.biases]
@@ -170,8 +181,7 @@ class Network:
                 for batch in mini_batches:
                     momentum_b, momentum_w = self.nag_epoch_apply(batch, momentum_b, momentum_w, eta)
 
-                print(f"Epoch {i + 1} Completed")
-
+                complete_epoch(i)
         else:
             raise ValueError("Invalid Optimizer")
         
@@ -183,6 +193,10 @@ class Network:
             activation = non_linearity(z, self.non_linearity_function)
 
         return activation
+
+    def evaluate(self, test: list[(np.ndarray, np.ndarray)]):
+        test_results = [(np.argmax(self.eval(x)), np.argmax(y)) for x, y in test]
+        return sum(int(x == y) for x, y in test_results)/len(test_results)
                 
 
 if __name__ == "__main__":
---
+++ README.md
@@ -0,0 +1,28 @@
+# Readme
+
+This is a simple python implementation of a neural network, in Network.py
+
+The network is generic, one can specify:
+
+```python
+net = Network([10, 20, 30])
+```
+
+And that will create a network with 10 neurons in the first layer, 20 in the second, and 30 in the last layer.
+
+We can then train it using stochastic gradient descent: For each epoch, the training data is split in mini-batches, a gradient of the network's weights and biases is calculated for the inputs and outputs of each mini-batch, the gradients are averaged out and the weights and biases are moved in the opposite direction, a distance equal to `eta`.
+
+1. `data` is expected to be a list of tuples of ndarrays
+2. `test` is expected to have the same type as `data`, it is optional: if supplied, accuracy will be measured after the application of each epoch
+
+The first layer will be expected to have the same length as the first elements of the elements of the `data` and `test` parameters, and the last layer, with the second elements, i.e. the first layer is interpreted as the input layer and the last layer as the output layer.
+
+It is recommended to normalize the data to avoid numerical errors.
+
+```python
+net.SGD_train(data=train, epochs=30, eta=2, mini_batch_size=50, test=test)
+```
+
+The default optimizer is `constant_eta`, which simply means that the average gradient calculated for each epoch will be normalized, multiplied by the original eta supplied in `SGD_Train` and added to the weights and biases on each epoch. Other optimizers were implemented, namely `nag` and `momentum`, however, after testing, the results with these were worse than using `constant_eta`
+
+In `test.ipynb`, the results of testing some networks with the MNIST Handwritten numbers dataset are shown.
